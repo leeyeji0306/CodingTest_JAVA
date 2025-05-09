ebf54a26aafd3a872acfd54b271bdf1be7282d53
y2_x1l <s2548@e-mirim.hs.kr>
2025-05-09 18:14:12 +0900

    [Bronze V] Title: A/B, Time: 192 ms, Memory: 18004 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main { 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1, num2;
+		num1 = sc.nextDouble();
+		num2 = sc.nextDouble();
+		System.out.print(num1/num2);
+	}
+}
