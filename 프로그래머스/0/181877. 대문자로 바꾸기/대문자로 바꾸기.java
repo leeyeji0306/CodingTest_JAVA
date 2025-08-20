bc2a55b174c21d870cf8fcec21e1e17917d0a178
y2_x1l <s2548@e-mirim.hs.kr>
2025-08-21 08:22:56 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 9.93 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i=0; i<myString.length(); i++){
+            if(myString.charAt(i)>=97){
+                answer+=(char)(myString.charAt(i)-32);
+            }
+            else{
+                answer+=myString.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
