e0d9208385f7c8359e464e3afbad125429926111
y2_x1l <s2548@e-mirim.hs.kr>
2026-01-25 15:13:05 +0900

    [level 1] Title: 소수 만들기, Time: 22.18 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++){
+            for(int j=i; j<nums.length-2; j++){
+                for(int k=j; k<nums.length-2; k++){
+                    int temp=nums[i]+nums[j+1]+nums[k+2];
+                    int flag=0;
+                    for(int l=2; l<temp; l++){
+                        if(temp%l==0){
+                            flag++;
+                            break;
+                        }
+                    }
+                    if(flag==0){
+                        answer++;
+                    }
+                }
+            }    
+        }
+
+        return answer;
+    }
+}
